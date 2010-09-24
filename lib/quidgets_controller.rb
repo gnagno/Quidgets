a046c77301fd78ce3803278cc3505f60eb48dcc1
Phillip Phan <phtphan@gmail.com>
2010-09-23 19:55:17 -0700

    improve

--- lib/quidgets_controller.rb
+++ lib/quidgets_controller.rb
@@ -8,26 +8,31 @@ module QuidgetsController
   #/////////////////////////////////////////////////////////////////////////////////////////////////////
   module ClassMethods
     def enable_quidgets
-      include PhillipWidgetsController::InstanceMethods
+      include QuidgetsController::InstanceMethods
     end
   end
 
   #/////////////////////////////////////////////////////////////////////////////////////////////////////
   module InstanceMethods
       ###################################################### quidgets_checkbox_update    
-      def phillip_widgets_checkbox_update 
+      def quidgets_checkbox_update 
         model=params[:model_name].camelize.constantize.find(params[:id])
-        model.update_attribute(params[:field_name].to_sym,params[:svalue])
+        if params[:svalue]=="true"
+          model.update_attribute(params[:field_name].to_sym,1)
+        elsif params[:svalue]=="false"
+          model.update_attribute(params[:field_name].to_sym,0)          
+        end
         model.save!
         render :text => ""
       end
       ###################################################### quidgets_radio_update    
-      def phillip_widgets_radio_update
+      def quidgets_radio_update
         model=params[:model].camelize.constantize.find(params[:id])
         group_id=model.send(params[:parent_field].to_sym)
         params[:model].camelize.constantize.update_all("#{params[:field]}=0","#{params[:parent_field]}='#{group_id}'")
         model.update_attribute(params[:field].to_sym,1)
         model.save!      
+        render :text => ""
       end    
   end    
   #/////////////////////////////////////////////////////////////////////////////////////////////////////
--- lib/quidgets_helper.rb
+++ lib/quidgets_helper.rb
@@ -1,24 +1,50 @@
-module PhillipWidgetsHelper
+module QuidgetsHelper
 ###################################################################################### checkbox  
-  def checkbox(params) # :html_id, :model, :id, :field
-    model=params[:model].camelize.constantize.find(params[:id])
-    html="<input type=\"checkbox\" id=\"#{params[:html_id]}\" "
-    html << "onclick=\"#{remote_function(:url => "/application/quidgets_checkbox_update?id=#{params[:id]}&model_name=#{params[:model]}&field_name=#{params[:field]}", :with => "'svalue='+ this.checked")}\""
-    if model.send(params[:field].to_sym)==true
+  def checkbox(params) # :id, :record, :field
+    #model=params[:model].camelize.constantize.find(params[:id])
+    #Fine model name for data instance
+    params[:record].to_s.match(/<.*:/)
+    model_name=$&.gsub(/<|:/,"")
+    
+    
+    html="<input type=\"checkbox\" id=\"#{params[:id]}\" "
+    html << "onclick=\"#{remote_function(:url => "/application/quidgets_checkbox_update?id=#{params[:record].id}&model_name=#{model_name}&field_name=#{params[:field]}", :with => "'svalue='+ this.checked")}\""
+    if params[:record].send(params[:field].to_sym)==1
       html << " checked "
     end
     html << "/>"
     return html
   end
+###################################################################################### checkbox_group  
+  def checkbox_group(params) # :id, :records, :field
+    html=""
+    params[:records].each do |record|
+      html << checkbox(:id => params[:id], :record => record, :field => params[:field])
+    end
+    return html
+  end
+
 ###################################################################################### radio
-  def radio(params) # :html_id, :model, :id, :field, :parent_field    
-    model=model_name.camelize.constantize.find(params[:id])    
-    html="<input type=\"radio\" id=\"#{params[:html_id]}\" "
-    if model.send(params[:field].to_sym)==true
+  def radio(params) # :name, :record, :field, :reference_field    
+    #Fine model name for data instance
+    params[:record].to_s.match(/<.*:/)
+    model_name=$&.gsub(/<|:/,"")
+    
+    #Construct HTML element
+    html="<input type=\"radio\" name=\"#{params[:name]}\" "
+    if params[:record].send(params[:field].to_sym)==1
       html << "checked "
     end
-    html << "onclick=\"#{remote_function(:url => "/application/quidgets_radio_update?id=#{params[:id]}&model=#{params[:model]}&field=#{params[:field]}&parent_field=#{params[:parent_field]}", :with => "'svalue='+ this.checked")}\""
+    html << "onclick=\"#{remote_function(:url => "/application/quidgets_radio_update?id=#{params[:record].id}&model=#{model_name}&field=#{params[:field]}&parent_field=#{params[:reference_field]}", :with => "'svalue='+ this.checked")}\""
     html << "/>"
+  end
+###################################################################################### radio_group
+  def radio_group(params) # :name, :records, :field, :reference_field
+    html=""
+    params[:records].each do |record|
+      html << radio(:name => params[:name], :record => record, :field => params[:field], :reference_field => params[:reference_field])
+    end
+    return html
   end
 ######################################################################################
 end
