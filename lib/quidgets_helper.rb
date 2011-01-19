ed2173017c76f5f85d5b2d4bbe08a18324de01dc
Phillip Phan <phtphan@gmail.com>
2011-01-19 11:29:49 -0800

    progress

--- lib/quidgets_helper.rb
+++ lib/quidgets_helper.rb
@@ -59,33 +59,37 @@ module QuidgetsHelper
       value=\"#{object.send(method.to_sym)}\"/>"        
   end
 ###################################################################################### listbox
-#* LISTBOX: listbox(instance,choices,html_options = {})
+#* LISTBOX: listbox(object,choices,html_options = {})
 #    <%= listbox(@user,PaymentMethod.all,{:id => "dropbox_#{@user.id}"}) %>        
 
-  def listbox(instance,choices,html_options = {})
+  def listbox(object,choices,html_options = {})
     
   end
 ###################################################################################### dropbox
-#* DROPBOX: dropbox(instance,choices,html_options = {})
+#* DROPBOX: dropbox(object,choices,html_options = {})
 # dropbox(user,Role.all,{:id => "role_#{user.id}"})
 
-  def dropbox(instance,choices,html_options = {})
-    model_name=find_model_name(instance)
+  def dropbox(object,choices,html_options = {})
+    model_name=find_model_name(object)
     html="<select 
       id=\"#{html_options[:id]}\" 
       name=\"#{html_options[:name]}\" 
       class=\"#{html_options[:class]}\" 
       />"
     
-    html << "<option onclick=\"#{remote_function(:url => "/application/quidgets_dropbox_update/#{instance.id}?model_name=#{model_name}&option_model_name=#{find_model_name(choices.first).underscore}")}\">(Please select)</option>"
+    html << "<option onclick=\"#{remote_function(:url => "/application/quidgets_dropbox_update/#{object.id}?model_name=#{model_name}&option_model_name=#{find_model_name(choices.first).underscore}")}\">(Please select)</option>"
       
     choices.each do |choice|
-      html << "<option onclick=\"#{remote_function(:url => "/application/quidgets_dropbox_update/#{instance.id}?model_name=#{model_name}&option_model_name=#{find_model_name(choice).underscore}&option_id=#{choice.id}")}\"" <<
-      "#{"selected" if !instance.send(find_model_name(choice).underscore.to_sym).nil? and instance.send(find_model_name(choice).downcase.to_sym).id==choice.id} >" << 
+      html << "<option onclick=\"#{remote_function(:url => "/application/quidgets_dropbox_update/#{object.id}?model_name=#{model_name}&option_model_name=#{find_model_name(choice).underscore}&option_id=#{choice.id}")}\"" <<
+      "#{"selected" if !object.send(find_model_name(choice).underscore.to_sym).nil? and object.send(find_model_name(choice).downcase.to_sym).id==choice.id} >" << 
       "#{choice.name}</option>"
     end
     html <<"</select>"
     return html
+  end
+###################################################################################### textbox
+  def textbox(object,field,html_options = {})
+    render :file => 'views/textbox.html.erb'
   end
 
 ######################################################################################  PRIVATE
